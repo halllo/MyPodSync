b7b05850c3e49520638d3f0bb26275eecf409e25
Manuel Naujoks <mnaujoks@gmail.com>
2025-03-22 21:10:03 +0100

    run script

--- run.ps1
+++ run.ps1
@@ -1,14 +1,21 @@
 # Get podman ready
 podman machine start
 
-# Run the container for 30 minutes (1800 seconds) and then stop it
-Start-Job -ScriptBlock {
-    podman run -p 5055:8080 --rm -it -v D:\MyPodSync\acishare:/app/data/ -e "PODSYNC_CONFIG_PATH=/app/data/config.toml" ghcr.io/mxpv/podsync:latest
-} | Out-Null
-
-Start-Sleep -Seconds 1800
+# Run the container for 10 minutes (600 seconds) and then stop it
+Start-Process -NoNewWindow -FilePath podman -ArgumentList @(
+    "run",
+    "-p", "5055:8080",
+    "--rm",
+    "-it",
+    "-v", "D:\MyPodSync\acishare:/app/data/",
+    "-e", "PODSYNC_CONFIG_PATH=/app/data/config.toml",
+    "ghcr.io/mxpv/podsync:latest"
+)
+Start-Sleep -Seconds 600
 podman stop $(podman ps -q --filter ancestor=ghcr.io/mxpv/podsync:latest)
 
 # Sync to the cloud
+$CurrentDir = Get-Location
 cd D:\MyPodSync\MyPodSync\MyPodSync.Sync\bin\Debug\net8.0
-MyPodSync.Sync.exe sync --source "D:\MyPodSync\acishare\files"
+.\MyPodSync.Sync.exe sync --source "D:\MyPodSync\acishare\files"
+cd $CurrentDir
